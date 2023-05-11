ec96c03bdf133eb7bbfaede8880556e2b82f62a4
Bharat Parihariya <bharat parihariya704@gmail.com>
2023-05-11 11:32:07 +0530

    Meme App

---
+++ Meme Generator/script.js
@@ -0,0 +1,44 @@
+const generateMemeBtn = document.querySelector(".meme-generator .meme-btn");
+
+const memeImage = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeAuthor = document.querySelector(".meme-generator .meme-author");
+
+const updateDetails = (url,title,author) => {
+    memeImage.setAttribute("src",url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+};
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+         updateDetails(data.url, data.title,data.author);
+       
+       
+    });
+        const u_data = data.url;
+        document.getElementById("img").src = u_data;
+}; 
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme();
+
+function share() {
+    if(navigator.share !== undefined){
+        navigator
+        .share({
+            // title: "Memes Generator",
+            // text:"Memes Generator",
+            // url:"u_data",
+             url:"u_data",
+        })
+
+        .then(()=>console.log("Successfull Share"))
+        .catch((error)=>console.log("Error",error))
+    }
+}
+share();
+
